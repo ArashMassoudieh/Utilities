c28a85e10f62c81b43f3f5c48f882ae1cc220cbb
Arash <arashmassoudieh@gmail.com>
2022-05-13 12:34:34 -0400

    Pathmaker

--- Distribution.h
+++ Distribution.h
@@ -27,7 +27,6 @@ public:
     static bool HasCommand(const string &cmd);
     vector<string> commands();
     static vector<string> Commands();
-    static vector<string> list_of_commands;
     double pi;
 	int n;
 	vector<double> s;
